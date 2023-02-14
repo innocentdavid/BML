f52816ec7c601012866abd2916cb238875b2979c
shadow <olatfajumo@gmail.com>
2023-02-14 05:54:22 -0800

    responsive fixed

---
+++ header.js
@@ -0,0 +1,16 @@
+function myfunction() {
+    let sub = document.getElementById('sub');
+    if (sub.style.display=="block"){
+        sub.style.display="None";
+    }else{
+        sub.style.display= "block";
+    }  
+}
+function myfunctions() {
+    let sub = document.getElementById('down');
+    if (sub.style.display=="block"){
+        sub.style.display="None";
+    }else{
+        sub.style.display= "block";
+    }  
+}
