e2fd578eead477df2ced86ca4ea7c268d291eee1
shadow <olatfajumo@gmail.com>
2023-02-15 05:55:35 -0800

    artist page

--- artist.js
+++ artist.js
@@ -1,7 +1,7 @@
 const dots = document.querySelectorAll(".dot-container button");
 const images = document.querySelectorAll(".image-container img");
 let i = 0;
-let j = 4
+let j = 2
 function next(){
     document.getElementById("content" + (i+1)).classList.remove("active");
     i = ( j + i + 1) % j;
