e918b4e102c29fd61c1d6555d1109c40ecba3632
innocentdavid <paulinnocent04@gmail.com>
2023-02-17 18:49:48 +0100

    hello

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,6 +29,9 @@ module.exports = {
 
             '2xl': '1536px',
             // => @media (min-width: 1536px) { ... }
+
+            '3xl': '1600px',
+            // => @media (min-width: 1536px) { ... }
         }
     },
     plugins: [],
